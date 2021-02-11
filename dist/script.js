e473e8a41e659310d571e843e7835b5a95aa5042
Korshukov Stepan <korshukovstepan@gmail.com>
2021-02-11 20:00:50 +0300

    add head slider

--- dist/script.js
+++ dist/script.js
@@ -93,7 +93,16 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+$(".top-slider").slick({
+  autoplay: true,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  arrow: false
+});
+$(".top-slider .slick-arrow").remove();
 
 /***/ })
 
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(".top-slider").slick({
+  autoplay: true,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  arrow: false,
+});
+$(".top-slider .slick-arrow").remove();
