89c8287267774be7eb6d6919c50dfe277165cb76
Korshukov Stepan <korshukovstepan@gmail.com>
2021-02-18 11:46:33 +0300

    work with adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,24 @@ $(".top-slider").slick({
   arrow: false,
 });
 $(".top-slider .slick-arrow").remove();
+
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".top__list"),
+    submenu = document.querySelector(".bottom__list"),
+    menuItem = document.querySelectorAll(".top__item"),
+    hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("top__list_active");
+    submenu.classList.toggle("bottom__list_active");
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("top__list_active");
+      submenu.classList.toggle("bottom__list_active");
+    });
+  });
+});
