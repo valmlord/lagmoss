eea2d7d20adb38c55483015a697ad95412994220
Korshukov Stepan <korshukovstepan@gmail.com>
2021-02-19 11:20:24 +0300

    adaptive menu

--- dist/script.js
+++ dist/script.js
@@ -1616,28 +1616,36 @@ $(".top-slider").slick({
 });
 $(".top-slider .slick-arrow").remove();
 window.addEventListener("DOMContentLoaded", function () {
-  var topMenu = document.querySelector(".header__top"),
-      topMenuItem = document.querySelector(".top__link"),
-      subMenu = document.querySelector(".bottom__list"),
+  var subMenu = document.querySelector(".bottom__list"),
       subMenuItem = document.querySelectorAll(".bottom__link"),
+      topMenu = document.querySelector(".header__top"),
+      topMenuItem = document.querySelectorAll(".top__link"),
+      socialIcon = document.querySelectorAll(".social__icon"),
       hamburger = document.querySelector(".hamburger");
   hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("hamburger_active");
-    topMenu.classList.toggle("header__top_active");
     subMenu.classList.toggle("bottom__list_active");
-  });
-  subMenuItem.forEach(function (item) {
-    item.addEventListener("click", function () {
-      hamburger.classList.toggle("hamburger_active");
-      topMenu.classList.toggle("header__top_active");
-      subMenu.classList.toggle("bottom__list_active");
-    });
+    topMenu.classList.toggle("header__top_active");
   });
   topMenuItem.forEach(function (item) {
     item.addEventListener("click", function () {
       hamburger.classList.toggle("hamburger_active");
-      topMenu.classList.toggle("header__top");
       subMenu.classList.toggle("bottom__list_active");
+      topMenu.classList.toggle("header__top_active");
+    });
+  });
+  subMenuItem.forEach(function (item) {
+    item.addEventListener("click", function () {
+      hamburger.classList.toggle("hamburger_active");
+      subMenu.classList.toggle("bottom__list_active");
+      topMenu.classList.toggle("header__top_active");
+    });
+  });
+  socialIcon.forEach(function (item) {
+    item.addEventListener("click", function () {
+      hamburger.classList.toggle("hamburger_active");
+      subMenu.classList.toggle("bottom__list_active");
+      topMenu.classList.toggle("header__top_active");
     });
   });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -10,31 +10,40 @@ $(".top-slider").slick({
 $(".top-slider .slick-arrow").remove();
 
 window.addEventListener("DOMContentLoaded", () => {
-  const topMenu = document.querySelector(".header__top"),
-    topMenuItem = document.querySelector(".top__link"),
-    subMenu = document.querySelector(".bottom__list"),
+  const subMenu = document.querySelector(".bottom__list"),
     subMenuItem = document.querySelectorAll(".bottom__link"),
+    topMenu = document.querySelector(".header__top"),
+    topMenuItem = document.querySelectorAll(".top__link"),
+    socialIcon = document.querySelectorAll(".social__icon"),
     hamburger = document.querySelector(".hamburger");
 
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger_active");
-    topMenu.classList.toggle("header__top_active");
     subMenu.classList.toggle("bottom__list_active");
-  });
-
-  subMenuItem.forEach((item) => {
-    item.addEventListener("click", () => {
-      hamburger.classList.toggle("hamburger_active");
-      topMenu.classList.toggle("header__top_active");
-      subMenu.classList.toggle("bottom__list_active");
-    });
+    topMenu.classList.toggle("header__top_active");
   });
 
   topMenuItem.forEach((item) => {
     item.addEventListener("click", () => {
       hamburger.classList.toggle("hamburger_active");
-      topMenu.classList.toggle("header__top");
       subMenu.classList.toggle("bottom__list_active");
+      topMenu.classList.toggle("header__top_active");
+    });
+  });
+
+  subMenuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      subMenu.classList.toggle("bottom__list_active");
+      topMenu.classList.toggle("header__top_active");
+    });
+  });
+
+  socialIcon.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      subMenu.classList.toggle("bottom__list_active");
+      topMenu.classList.toggle("header__top_active");
     });
   });
 });
