8c80283bcdf4dab1272e22d74aca58a7ff0f14a1
Korshukov Stepan <korshukovstepan@gmail.com>
2021-02-23 19:15:07 +0300

    add product page

--- dist/script.js
+++ dist/script.js
@@ -1609,10 +1609,11 @@ $(".main-slider").slick({
   autoplay: true,
   dots: true,
   infinite: true,
-  speed: 200,
+  speed: 500,
   slidesToShow: 1,
   adaptiveHeight: true,
-  arrow: false
+  arrow: false,
+  fade: true
 });
 $(".main-slider .slick-arrow").remove();
 window.addEventListener("DOMContentLoaded", function () {
@@ -1636,6 +1637,30 @@ window.addEventListener("DOMContentLoaded", function () {
       menu.classList.toggle("mobile_active");
     });
   });
+});
+$(".product-card__slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: false,
+  asNavFor: ".slider-nav-thumbnails"
+});
+$(".product-card__nav-thumbnails").slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: ".product-card__slider",
+  dots: false,
+  focusOnSelect: true
+}); // Remove active class from all thumbnail slides
+
+$(".product-card__nav-thumbnails .slick-slide").removeClass("slick-active"); // Set active class to first thumbnail slides
+
+$(".product-card__nav-thumbnails .slick-slide").eq(0).addClass("slick-active"); // On before slide change match active thumbnail to current slide
+
+$(".product-card__slider").on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+  var mySlideNumber = nextSlide;
+  $(".product-card__nav-thumbnails .slick-slide").removeClass("slick-active");
+  $(".product-card__nav-thumbnails .slick-slide").eq(mySlideNumber).addClass("slick-active");
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,11 @@ $(".main-slider").slick({
   autoplay: true,
   dots: true,
   infinite: true,
-  speed: 200,
+  speed: 500,
   slidesToShow: 1,
   adaptiveHeight: true,
   arrow: false,
+  fade: true,
 });
 $(".main-slider .slick-arrow").remove();
 
@@ -34,3 +35,37 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+$(".product-card__slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: false,
+  asNavFor: ".slider-nav-thumbnails",
+});
+
+$(".product-card__nav-thumbnails").slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: ".product-card__slider",
+  dots: false,
+  focusOnSelect: true,
+});
+
+// Remove active class from all thumbnail slides
+$(".product-card__nav-thumbnails .slick-slide").removeClass("slick-active");
+
+// Set active class to first thumbnail slides
+$(".product-card__nav-thumbnails .slick-slide").eq(0).addClass("slick-active");
+
+// On before slide change match active thumbnail to current slide
+$(".product-card__slider").on(
+  "beforeChange",
+  function (event, slick, currentSlide, nextSlide) {
+    var mySlideNumber = nextSlide;
+    $(".product-card__nav-thumbnails .slick-slide").removeClass("slick-active");
+    $(".product-card__nav-thumbnails .slick-slide")
+      .eq(mySlideNumber)
+      .addClass("slick-active");
+  }
+);
